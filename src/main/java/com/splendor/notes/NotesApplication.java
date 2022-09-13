f92be035dfd9ec43acb7814bb0491e6661c17d32
孙辉 <sunhui@91160.com>
2022-09-13 18:56:39 +0800

    notes init

---
+++ src/main/java/com/splendor/notes/NotesApplication.java
@@ -0,0 +1,17 @@
+package com.splendor.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author splendor.s
+ * @create 2022-09-13 18:33
+ */
+@SpringBootApplication
+public class NotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotesApplication.class, args);
+    }
+
+}
