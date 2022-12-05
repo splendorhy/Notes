f8c794d8a50332d171744be2669d4d39680cd832
孙辉 <sunhui@91160.com>
2022-12-05 20:37:27 +0800

    公共响应码处理

---
+++ src/main/java/com/splendor/notes/infrastructure/util/basic/DataCsdn.java
@@ -0,0 +1,14 @@
+package com.splendor.notes.infrastructure.util.basic;
+
+import lombok.Data;
+
+/**
+ * @author splendor.s
+ * @create 2022/12/5 下午8:11
+ * @description
+ */
+@Data
+public class DataCsdn {
+    //private List<Article> list;
+    private Integer total;
+}
